c32b157e38c4c6a1994d445f4a7ef8c7b4daa721
azeenGAN <140174610+azeenGAN@users.noreply.github.com>
2024-01-09 13:31:35 +0500

    Add files via upload

---
+++ power.js
@@ -0,0 +1,102 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let var0 = document.querySelectorAll(".causalleavesallowed");
+    var0.forEach(function(column){
+    column.textContent = 12;
+    })
+});
+
+document.addEventListener('DOMContentLoaded', function(){
+let var1=document.querySelectorAll(".medicalleavesallowed")
+var1.forEach(function(column2){
+    column2.textContent=7
+})
+});
+document.addEventListener('DOMContentLoaded', function(){
+let var1=document.querySelectorAll(".earnedleavebalance")
+var1.forEach(function(column2){
+    column2.textContent=10
+})
+});
+document.addEventListener('DOMContentLoaded', function(){
+let var1=document.querySelectorAll(".earnedleaveallowed")
+var1.forEach(function(column2){
+    column2.textContent=10
+})
+});
+document.addEventListener('DOMContentLoaded', function(){
+let var1=document.querySelectorAll(".status")
+var1.forEach(function(column2){
+    column2.textContent="confirm";
+})
+});
+document.addEventListener('DOMContentLoaded', function(){
+let var1=document.querySelectorAll(".casualleavesavailed")
+var1.forEach(function(column2){
+    column2.textContent=0;
+})
+});
+document.addEventListener('DOMContentLoaded', function(){
+let var1=document.querySelectorAll(".medicalleavesavailed")
+var1.forEach(function(column2){
+    column2.textContent=0;
+})
+});
+// Get the screen width
+var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+// Log the screen width to the console
+console.log("Screen Width: " + screenWidth + " pixels");
+
+// class Person{
+    
+//     constructor(name = "Tom", age = 20, energy = 100) {
+//         this.name = name;
+//         this.age = age;
+//         this.energy = energy;
+//     }
+
+//     sleep() {
+//         this.energy += 10
+//     }
+//     doSomethingFun() {
+//         this.energy -= 10
+//     }
+// }
+// // Task 2: Code a Worker class
+// class Worker extends Person{
+//     constructor(xp = 0, hourlyWage = 10, name="raja", age=17, energy=18) {
+//         super(name, age, energy)
+//         this.xp = xp;
+//         this.hourlyWage=hourlyWage
+//     }
+// }
+
+// var ww=new Worker()
+// console.log(ww.xp);
+// console.log(ww.hourlyWage);
+// console.log(ww.name);
+// console.log(ww.age);
+// console.log(ww.energy);
+
+
+
+
+
+// Task 1: Code a Person class
+// class Person{
+    
+  
+//     sleep() {
+//         console.log("sleep")
+//     }
+//     doSomethingFun() {
+//         console.log("Fun")
+//     }
+// }
+
+// let tt= new Person();
+// tt.sleep();
+// tt. doSomethingFun()
+//  ;
+
+
